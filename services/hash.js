6261eb25c5ba202861de20661b5e266db4fa200a
Mohamed Hany <MohamedEgy246@gmail.com>
2025-12-01 22:29:53 +0200

    implement hash service

---
+++ services/hash.js
@@ -0,0 +1,9 @@
+const bcrypt = require("bcryptjs");
+
+const hashPassword = async (plainPassword) => {
+  return await bcrypt.hash(plainPassword, 10);
+};
+const comparePassword = async (plainPassword, hashedPassword) => {
+  return await bcrypt.compare(plainPassword, hashedPassword);
+};
+module.exports = { hashPassword, comparePassword };
